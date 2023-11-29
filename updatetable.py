349520e6aac5a2487f16897787d713791c598a16
Sharath Palthepu <dahurley56@gmail.com>
2023-11-29 14:39:06 -0600

    added zipcode column to dataframe

--- updatetable.py
+++ updatetable.py
@@ -1,10 +1,33 @@
 import pandas as pd
-
+import googlemaps
+from geopy.geocoders import GoogleV3
 
 df = pd.read_csv('chicago_apartments.csv')
 
 # Create a new DataFrame to store the updated data
 new_rows = []
+
+# create API key using googlemaps geocoding API
+API_KEY = 'AIzaSyAScCshi5dAcLkJaSFcO_i9QIkcT0IUnDc'
+gmaps = googlemaps.Client(key=API_KEY)
+
+# create function to extract zipcode from a given address
+def get_zipcode(address):
+    try:
+        # Geocode the address
+        geocode_result = gmaps.geocode(address)
+
+        # Extract the zip code from the result
+        if geocode_result and 'address_components' in geocode_result[0]:
+            for component in geocode_result[0]['address_components']:
+                if 'postal_code' in component['types']:
+                    return component['long_name']
+
+        # Return None if no zip code found
+        return None
+    except Exception as e:
+        print(f"Error processing address {address}: {e}")
+        return None
 
 # Iterate through each row
 for index, row in df.iterrows():
@@ -18,6 +41,7 @@ for index, row in df.iterrows():
 
 # Create a new DataFrame with the updated rows
 new_df = pd.DataFrame(new_rows)
+new_df['Zipcode'] = new_df['Address'].apply(get_zipcode)
 
 # Save the new DataFrame to a new CSV file
 new_df.to_csv('transform_apt_data.csv', index=False)
